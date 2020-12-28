949f20b6e78af4904dccbdc21e8ec176381f3ab6
Jameel Al-Aziz <me@jalaziz.io>
2020-12-28 15:59:54 -0800

    Cleanup auto_create_rfqs scripts

--- api/auto_create_rfqs/auto_create_rfqs.py
+++ api/auto_create_rfqs/auto_create_rfqs.py
@@ -13,8 +13,6 @@ Environment Variables:
     PARADIGM_ACCOUNT_NAME_CME: Paradigm Account Name - CME
     PARADIGM_DESK_NAME:        Paradigm Desk Name
     MAKER_DESK_NAME:           Maker Desk Name
-    DBT_HTTP_HOST:             HTTP Host - DBT
-    BIT_HTTP_HOST:             HTTP Host - BIT
     PARADIGM_WS_URL:           Paradigm WS URL
     PARADIGM_HTTP_HOST:        Paradigm HTTP Host
 
@@ -31,9 +29,9 @@ import random
 import requests
 import sys
 import time
-import websockets
-
 from urllib.parse import urljoin
+
+import websockets
 
 
 try:
@@ -45,18 +43,173 @@ except IndexError:
 
 RFQ_CREATE_SECONDS = 10
 
+SCENARIOS = [
+    {
+        'anonymous': True,
+        'expires_in': 60,
+        'legs': [
+            {
+                'quantity': 20000,
+                'side': 'BUY',
+                'instrument': 'BTC-PERPETUAL',
+                'venue': 'DBT',
+            },
+        ]
+    },
+    {
+        'expires_in': 80,
+        'legs': [
+            {
+                'quantity': 15000,
+                'side': 'BUY',
+                'instrument': 'BTC-PERPETUAL',
+                'venue': 'DBT',
+            },
+            {
+                'quantity': 15000,
+                'side': 'SELL',
+                'instrument': 'BTC-26MAR21',
+                'venue': 'DBT',
+            },
+        ]
+    },
+     {
+        'expires_in': 60,
+        'legs': [
+            {
+                'quantity': 50000,
+                'side': 'BUY',
+                'instrument': 'BTC-26MAR21',
+                'venue': 'DBT',
+            },
+            {
+                'quantity': 50000,
+                'side': 'SELL',
+                'instrument': 'BTC-PERPETUAL',
+                'venue': 'DBT',
+            },
+        ]
+    },
+    {
+        'expires_in': 60,
+        'legs': [
+            {
+                'quantity': 45000,
+                'side': 'SELL',
+                'instrument': 'BTC-PERPETUAL',
+                'venue': 'DBT',
+            },
+            {
+                'quantity': 45000,
+                'side': 'BUY',
+                'instrument': 'BTC-25JUNE21',
+                'venue': 'DBT',
+            },
+        ]
+    },
+    {
+        'expires_in': 65,
+        'legs': [
+            {
+                'quantity': 25,
+                'side': 'BUY',
+                'instrument': 'BTC-8JAN21-22000-C',
+                'venue': 'DBT',
+            },
+        ]
+    },
+    {
+        'expires_in': 65,
+        'legs': [
+            {
+                'quantity': 12.5,
+                'side': 'BUY',
+                'instrument': 'BTC-8JAN21-22000-C',
+                'venue': 'DBT',
+            },
+            {
+                'quantity': 12.5,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-18000-C',
+                'venue': 'DBT',
+            },
+        ]
+    },
+    {
+        'expires_in': 65,
+        'legs': [
+            {
+                'quantity': 12.5,
+                'side': 'BUY',
+                'instrument': 'BTC-8JAN21-22000-C',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 12.5,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-18000-C',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 5,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-18000-P',
+                'venue': 'DBT'
+            }
+        ]
+    },
+    {
+        'expires_in': 65,
+        'legs': [
+            {
+                'quantity': 12.5,
+                'side': 'BUY',
+                'instrument': 'BTC-8JAN21-22000-C',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 12.5,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-18000-C',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 15,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-18000-P',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 50,
+                'side': 'SELL',
+                'instrument': 'BTC-29JAN21-15000-C',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 6,
+                'side': 'SELL',
+                'instrument': 'BTC-26MAR21-20000-P',
+                'venue': 'DBT'
+            },
+            {
+                'quantity': 63,
+                'side': 'SELL',
+                'instrument': 'BTC-25JUN21-20000-C',
+                'venue': 'DBT'
+            }
+        ]
+    },
+]
 
-async def main(
-    access_key, secret_key, paradigm_account_information,
-    paradigm_ws_url, dbt_http_host, bit_http_host,
-    paradigm_http_host, maker_desk_name,
-):
+
+async def main(http_host, ws_url, access_key, secret_key, maker_desk_name,
+               account_info):
     """
     Opens the websocket connection and starts creating RFQs.
 
     Initially subscribes to all notification channels.
     """
-    async with websockets.connect(f'{paradigm_ws_url}?api-key={access_key}') as websocket:
+    async with websockets.connect(f'{ws_url}?api-key={access_key}') as websocket:
         loop = asyncio.get_event_loop()
         # Start the heartbeat thread
         loop.create_task(send_heartbeat(websocket))
@@ -72,12 +225,11 @@ async def main(
 
         # Create RFQ Task that randomly sends out scenarios
         loop.create_task(create_rfq_task(
-            maker_desk_name=maker_desk_name,
-            dbt_http_host=dbt_http_host,
-            bit_http_host=bit_http_host,
+            http_host=http_host,
             access_key=access_key,
             secret_key=secret_key,
-            paradigm_http_host=paradigm_http_host,
+            maker_desk_name=maker_desk_name,
+            account_info=account_info,
         ))
 
         while True:
@@ -91,10 +243,8 @@ async def main(
                         continue
 
 
-async def create_rfq_task(
-    maker_desk_name, dbt_http_host, bit_http_host,
-    access_key, secret_key, paradigm_http_host,
-):
+async def create_rfq_task(http_host, access_key, secret_key, maker_desk_name,
+                          account_info):
     """
     Task that continually runs and creates an RFQ every `RFQ_CREATE_SECONDS` seconds
     using a random scenario from below every time.
@@ -102,187 +252,21 @@ async def create_rfq_task(
     while True:
         await asyncio.sleep(5)
         random_client_order_id = int(time.time())
-        scenario_payload = {
+        scenario_base_payload = {
             'account': {
-                'name': paradigm_account_information['name']['DBT']
+                'name': account_info['name']['DBT']
             },
             'anonymous': False,
             'client_order_id': random_client_order_id,
             'counterparties': [maker_desk_name],
         }
-        scenario_payload_1 = scenario_payload | {
-            'anonymous': True,
-            'expires_in': 60,
-            'legs': [
-                {
-                    'quantity': 20000,
-                    'side': 'BUY',
-                    'instrument': 'BTC-PERPETUAL',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_2 = scenario_payload | {
-            'expires_in': 80,
-            'legs': [
-                {
-                    'quantity': 15000,
-                    'side': 'BUY',
-                    'instrument': 'BTC-PERPETUAL',
-                    'venue': 'DBT',
-                },
-                {
-                    'quantity': 15000,
-                    'side': 'SELL',
-                    'instrument': 'BTC-26MAR21',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_3 = scenario_payload | {
-            'counterparties': [maker_desk_name],
-            'expires_in': 60,
-            'legs': [
-                {
-                    'quantity': 50000,
-                    'side': 'BUY',
-                    'instrument': 'BTC-26MAR21',
-                    'venue': 'DBT',
-                },
-                {
-                    'quantity': 50000,
-                    'side': 'SELL',
-                    'instrument': 'BTC-PERPETUAL',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_4 = scenario_payload | {
-            'expires_in': 60,
-            'legs': [
-                {
-                    'quantity': 45000,
-                    'side': 'SELL',
-                    'instrument': 'BTC-PERPETUAL',
-                    'venue': 'DBT',
-                },
-                {
-                    'quantity': 45000,
-                    'side': 'BUY',
-                    'instrument': 'BTC-25JUNE21',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_5 = scenario_payload | {
-            'expires_in': 65,
-            'legs': [
-                {
-                    'quantity': 25,
-                    'side': 'BUY',
-                    'instrument': 'BTC-8JAN21-22000-C',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_6 = scenario_payload | {
-            'expires_in': 65,
-            'legs': [
-                {
-                    'quantity': 12.5,
-                    'side': 'BUY',
-                    'instrument': 'BTC-8JAN21-22000-C',
-                    'venue': 'DBT',
-                },
-                {
-                    'quantity': 12.5,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-18000-C',
-                    'venue': 'DBT',
-                },
-            ]
-        }
-        scenario_payload_7 = scenario_payload | {
-            'expires_in': 65,
-            'legs': [
-                {
-                    'quantity': 12.5,
-                    'side': 'BUY',
-                    'instrument': 'BTC-8JAN21-22000-C',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 12.5,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-18000-C',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 5,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-18000-P',
-                    'venue': 'DBT'
-                }
-            ]
-        }
-        scenario_payload_8 = scenario_payload | {
-            'expires_in': 65,
-            'legs': [
-                {
-                    'quantity': 12.5,
-                    'side': 'BUY',
-                    'instrument': 'BTC-8JAN21-22000-C',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 12.5,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-18000-C',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 15,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-18000-P',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 50,
-                    'side': 'SELL',
-                    'instrument': 'BTC-29JAN21-15000-C',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 6,
-                    'side': 'SELL',
-                    'instrument': 'BTC-26MAR21-20000-P',
-                    'venue': 'DBT'
-                },
-                {
-                    'quantity': 63,
-                    'side': 'SELL',
-                    'instrument': 'BTC-25JUN21-20000-C',
-                    'venue': 'DBT'
-                }
-            ]
-        }
 
-        scenario_list = [
-            scenario_payload_1, scenario_payload_2, scenario_payload_3,
-            scenario_payload_4, scenario_payload_5, scenario_payload_6,
-            scenario_payload_7, scenario_payload_8,
-        ]
-        await create_rfq(
-            random.choice(scenario_list), dbt_http_host, bit_http_host,
-            access_key, secret_key, paradigm_http_host,
-        )
+        payload = scenario_base_payload | random.choice(SCENARIOS)
+        await create_rfq(http_host, access_key, secret_key, payload)
         await asyncio.sleep(RFQ_CREATE_SECONDS)
 
 
-async def create_rfq(
-    payload, dbt_http_host, bit_http_host,
-    access_key, secret_key, paradigm_http_host,
-):
+async def create_rfq(http_host, access_key, secret_key, payload):
     """
     Creates an RFQ using a signed request.
     """
@@ -308,7 +292,7 @@ async def create_rfq(
         'Authorization': f'Bearer {access_key}'
     }
 
-    response = requests.post(urljoin(paradigm_http_host, path), headers=headers, json=payload)
+    response = requests.post(urljoin(http_host, path), headers=headers, json=payload)
     return response
 
 
@@ -409,13 +393,6 @@ if __name__ == '__main__':
     MAKER_DESK_NAME = os.getenv(
         'MAKER_DESK_NAME', 'DSK1',
     )
-
-    DBT_HTTP_HOST = os.getenv(
-        'DBT_HTTP_HOST', 'https://test.deribit.com/api/v2',
-    )
-    BIT_HTTP_HOST = os.getenv(
-        'BIT_HTTP_HOST', 'https://testapi.bitexch.dev/v1',
-    )
     PARADIGM_WS_URL = os.getenv('PARADIGM_WS_URL', 'wss://ws.api.test.paradigm.co/')
     PARADIGM_HTTP_HOST = os.getenv('PARADIGM_HTTP_HOST', 'https://api.test.paradigm.co')
 
@@ -436,22 +413,18 @@ if __name__ == '__main__':
         print(f'Paradigm Account Name - CME: {PARADIGM_ACCOUNT_NAME_CME}')
         print(f'Paradigm Desk Name: {PARADIGM_DESK_NAME}')
         print(f'Maker Desk Name: {MAKER_DESK_NAME}')
-        print(f'HTTP Host - DBT: {DBT_HTTP_HOST}')
-        print(f'HTTP Host - BIT: {BIT_HTTP_HOST}')
         print(f'Paradigm WS URL: {PARADIGM_WS_URL}')
         print(f'Paradigm HTTP Host: {PARADIGM_HTTP_HOST}')
 
         # Start the client that opens a websocket connection and creates RFQs
         asyncio.get_event_loop().run_until_complete(
             main(
+                http_host=PARADIGM_HTTP_HOST,
+                ws_url=PARADIGM_WS_URL,
                 access_key=PARADIGM_ACCESS_KEY,
                 secret_key=PARADIGM_SECRET_KEY,
-                paradigm_account_information=paradigm_account_information,
-                dbt_http_host=DBT_HTTP_HOST,
-                bit_http_host=BIT_HTTP_HOST,
-                paradigm_ws_url=PARADIGM_WS_URL,
-                paradigm_http_host=PARADIGM_HTTP_HOST,
                 maker_desk_name=MAKER_DESK_NAME,
+                account_info=paradigm_account_information,
             )
         )
 
