b5b4355c4d23c2248e092a4f7858b9a3d57fa2d9
Jameel Al-Aziz <jameel@paradigm.co>
2021-01-14 04:13:45 -0800

    Update request signing script
    Update the request signing example script to be consistent with the
    updated API docs.

--- api/signature.py
+++ api/signature.py
@@ -1,70 +1,67 @@
 """
 Usage:
-   python3 signature.py <API KEY> <SECRET KEY>
+   python3 signature.py <ACCESS KEY> <SECRET KEY>
 """
 
 
 import base64
-import hashlib
 import hmac
 import json
-import time
-import requests
 import sys
+import time
+from urllib.parse import urljoin
+
+import requests
+
 
 # Use credentials from command line
 access_key = sys.argv[1]
 secret_key = sys.argv[2]
+host = 'https://api.test.paradigm.co'
 
-signing_key = base64.b64decode(secret_key)
+
+def sign_request(secret_key, method, path, body):
+    signing_key = base64.b64decode(secret_key)
+    
+    timestamp = str(int(time.time() * 1000)).encode('utf-8')
+    message = b'\n'.join([timestamp, method.upper(), path, body])
+    digest = hmac.digest(signing_key, message, 'sha256')
+    signature = base64.b64encode(digest)
+    
+    return timestamp, signature
 
 
 # GET example
-host = 'https://api.test.paradigm.co'
-method = 'GET'
-path = '/instruments/?page_size=1000'
+
+method = b'GET'
+path = b'/instruments/?page_size=1000'
 body = b''
-message = method.encode('utf-8') + b'\n'
-message += path.encode('utf-8') + b'\n'
-message += body
-timestamp = str(int(time.time() * 1000))
-timestamp = '1610359155205'
-message = timestamp.encode('utf-8') + b'\n' + message
-digest = hmac.digest(signing_key, message, 'sha256')
-signature = base64.b64encode(digest)
+
+timestamp, signature = sign_request(secret_key, method, path, body)
 headers = {
+    'Authorization': 'Bearer {}'.format(access_key),
     'Paradigm-API-Timestamp': timestamp,
     'Paradigm-API-Signature': signature,
-    'Authorization': 'Bearer ' + access_key
 }
-r = requests.get(host + path, headers=headers)
-print('GET response: ', r.json())
+url = urljoin(host, path.decode())
+response = requests.get(url, headers=headers)
+print('GET response: ', response.json())
 
 
 # POST example
-request_params = b''
-request_url = b'/echo/'
-request_method = b'POST'
-request_data = {'message': 'hello'}
-request_body = json.dumps(request_data).encode('utf-8')
 
-key = base64.b64decode(secret_key)
-timestamp = str(int(time.time() * 1000)).encode('utf-8')
+method = b'POST'
+path = b'/echo/'
+data = {'message': 'hello'}
+body = json.dumps(data).encode('utf-8')
 
-message = b'\n'.join([timestamp, request_method, request_url, request_body])
-digest = hmac.digest(key, message, hashlib.sha256)
-signature = base64.b64encode(digest)
-
+timestamp, signature = sign_request(secret_key, method, path, body)
 headers = {
-  'Accept': 'application/json',
-  'Authorization': 'Bearer ' + access_key,
-  'Paradigm-API-Signature': signature,
-  'Paradigm-API-Timestamp': timestamp
+    'Authorization': 'Bearer {}'.format(access_key),
+    'Paradigm-API-Signature': signature,
+    'Paradigm-API-Timestamp': timestamp,
+    'Accept': 'application/json',
 }
-
-r = requests.post(
-    'https://api.test.paradigm.co/echo/',
-    headers=headers,
-    json=request_data,
-)
-print('POST response: ', r.json())
+url = urljoin(host, path.decode())
+response = requests.post(url, headers=headers, json=data)
+print('POST response: ', response.json())
