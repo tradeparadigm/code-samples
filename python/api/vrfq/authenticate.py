68f2220e18670af57773c20eb8018d97ca595848
Steven <steven@ribbon.finance>
2022-04-04 20:01:29 +0800

    Added authenticate module

---
+++ python/api/vrfq/authenticate.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By: Steven (steven@ribbon.finance)
+# Created Date: 04/04/2022
+# version ='0.01'
+# ---------------------------------------------------------------------------
+""" Module to generate JWT signature """
+# ---------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+import jwt
+
+# ---------------------------------------------------------------------------
+# Constants
+# ---------------------------------------------------------------------------
+ALGORITHM = 'EdDSA'
+
+# ---------------------------------------------------------------------------
+# Authenticator
+# ---------------------------------------------------------------------------
+class Authenticator:
+  '''
+  Object to generate JWT signature
+
+  Args:
+      key (str): API key
+
+  Attributes:
+      key (str): API key in PKSI format
+  '''
+  def __init__(self, key: str):
+    self.key = f"-----BEGIN PRIVATE KEY-----\n{key}\n-----END PRIVATE KEY-----"
+
+  def signJWT(self, payload: dict):
+    '''
+    Method to generate JWT signature
+
+    Args:
+        payload (dict): payload to sign
+
+    Returns:
+        jwt (dict): JWT signature
+    '''
+    return jwt.encode(payload, self.key, algorithm=ALGORITHM)
