d29f75ceeb10b916bc2cdafc6e6e77de1e05be59
Mattia D'Antonio <mdantonio@users.noreply.github.com>
2022-09-01 15:31:42 +0200

    [vrfq] Changed code examples to adapt to the SDK common interface (#19)
    * [vrfq] Change code examples to adapt to the SDK common interface
    
    * Add missing dependency
    
    Co-authored-by: Cassios Marques <cassios.marques@paradigm.co>

--- python/api/vrfq/ribbon_api.py
+++ python/api/vrfq/ribbon_api.py
@@ -1,9 +1,7 @@
 from decimal import Decimal
 from pprint import pprint
 
-from ribbon.definitions import Bid
-from ribbon.wallet import Wallet
-from ribbon.definitions import Domain
+from ribbon.config import RibbonSDKConfig
 
 from paradigm_api import ParadigmClient
 
@@ -17,28 +15,29 @@ def sign_ribbon_bid(
     use_delegated_wallet=False,
 ) -> str:
     rfq_data = paradigm_client.get_rfq_data(rfq_id)
-    domain = Domain(
-        name=rfq_data['domain']['contract_name'],
-        chainId=rfq_data['domain']['chain_id'],
-        verifyingContract=rfq_data['domain']['verifying_contract'],
-        version=rfq_data['domain']['version'],
-    )
 
     bidding_data = paradigm_client.get_bidding_data(
-        rfq_id, price, wallet_name, use_nonce=True, use_delegated_wallet=use_delegated_wallet
+        rfq_id,
+        price,
+        wallet_name,
+        use_nonce=True,
+        use_delegated_wallet=use_delegated_wallet
     )
-    bid = Bid(
-        buyAmount=bidding_data['buy_amount'],
-        nonce=bidding_data['nonce'],
-        referrer=bidding_data['referrer'],
-        sellAmount=bidding_data['sell_amount'],
-        signerWallet=bidding_data['signer_wallet'],
-        swapId=bidding_data['swap_id'],
-    )
+
+    sdk = RibbonSDKConfig()
+
     print('Bid details to be signed:')
-    pprint(vars(bid))
+    pprint(bidding_data)
 
-    wallet = Wallet(private_key=wallet_private_key)
-    signed_bid = wallet.sign_bid(domain, bid)
-
-    return signed_bid.r[2:] + signed_bid.s[2:] + hex(signed_bid.v)[2:]
+    return sdk.sign_bid(
+        contract_address=rfq_data['domain']['verifying_contract'],
+        chain_id=rfq_data['domain']['chain_id'],
+        public_key=None,
+        private_key=wallet_private_key,
+        swap_id=bidding_data['swap_id'],
+        nonce=bidding_data['nonce'],
+        signer_wallet=bidding_data['signer_wallet'],
+        sell_amount=bidding_data['sell_amount'],
+        buy_amount=bidding_data['buy_amount'],
+        referrer=bidding_data['referrer'],
+    )
