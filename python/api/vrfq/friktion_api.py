e0204d3b4250e5874bfe8e00cf14c0acf8adc126
Mattia D'Antonio <m.dantonio81@gmail.com>
2022-09-01 13:06:37 +0200

    [vrfq] Change code examples to adapt to the SDK common interface

--- python/api/vrfq/friktion_api.py
+++ python/api/vrfq/friktion_api.py
@@ -1,12 +1,7 @@
 from decimal import Decimal
 from pprint import pprint
 
-
-from anchorpy import Wallet
-from friktion.bid_details import BidDetails
-from solana.publickey import PublicKey
-from solana.keypair import Keypair
-import solders.keypair
+from friktion.config import FriktionSDKConfig
 
 from paradigm_api import ParadigmClient
 
@@ -21,18 +16,18 @@ def sign_friktion_bid(
     bidding_data = paradigm_client.get_bidding_data(
         rfq_id, price, wallet_name, use_nonce=False, use_delegated_wallet=False
     )
-    bid = BidDetails(
-        bid_size=int(bidding_data['buy_amount']),
-        referrer=PublicKey(bidding_data['referrer']),
-        bid_price=int(bidding_data['sell_amount']) // int(bidding_data['buy_amount']),
-        signer_wallet=PublicKey(bidding_data['signer_wallet']),
-        order_id=int(bidding_data['swap_id']),
+
+    sdk = FriktionSDKConfig()
+    signature = sdk.sign_bid(
+        private_key=wallet_private_key,
+        swap_id=int(bidding_data['swap_id']),
+        signer_wallet=bidding_data['signer_wallet'],
+        sell_amount=int(bidding_data['sell_amount']),
+        buy_amount=int(bidding_data['buy_amount']),
+        referrer=bidding_data['referrer'],
     )
+
     print('Bid details to be signed:')
-    pprint(vars(bid))
+    pprint(bidding_data)
 
-    wallet = Wallet(Keypair(solders.keypair.Keypair.from_base58_string(wallet_private_key)))
-    msg, signature = bid.as_signed_msg(wallet)
-    print(f'Msg signed: {msg}')
-
-    return str(signature)
+    return signature
